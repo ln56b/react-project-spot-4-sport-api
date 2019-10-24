7e07956bbcb9e40a7f9d72c6b07b646d0613d190
Vi <dupre.virginie@gmail.com>
2019-10-24 12:14:25 +0200

    HomePage Component for first draft

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NineSports from './NineSports';
+
+function HomePage() {
+  return (
+    <div>
+      <NineSports />
+    </div>
+  );
+}
+
+export default HomePage;
