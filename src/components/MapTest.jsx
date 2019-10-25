4dd1dcdd599081d887938ef47e65929f7279c984
Guigzzz31 <guillaume.trouche.6@gmail.com>
2019-10-25 14:02:22 +0200

    eslint

--- src/components/MapTest.jsx
+++ src/components/MapTest.jsx
@@ -9,7 +9,7 @@ class MapTest extends React.Component {
   render() {
     return (
       <div>
-        <h3 style={{ textAlign: 'center' }}></h3>
+        <h3 style={{ textAlign: 'center' }} />
         <Map className="map" zoom={this.props.zoomCity} center={this.props.cityCenter}>
           <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           {data.city.map(i => {
