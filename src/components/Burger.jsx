b6f0c388c3c3c8c7ebf4c0e50f57a67dc2ae0876
Guigzzz31 <guillaume.trouche.6@gmail.com>
2019-10-25 12:25:52 +0200

    ajout style burger et searchbar

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-
+import './Burger.css';
 const Burger = props => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
