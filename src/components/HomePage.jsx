489b948a29a21edd11ce9b81ad5e3cc8b634d702
Vi <dupre.virginie@gmail.com>
2019-10-25 09:35:42 +0200

    save HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import NineSports from './NineSports';
+// import SportsList from './SportsList';
+// import DivStructure from './DivStructure';
 
 function HomePage() {
   return (
     <div>
       <NineSports />
+      {/* <SportsList />
+      <DivStructure /> */}
     </div>
   );
 }
