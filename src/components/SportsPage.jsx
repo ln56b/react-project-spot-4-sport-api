2c506bb04a8685345617d25a5aa937e6b182e1d8
Hélène Baudrand <baudrand.helene@gmail.com>
2019-10-25 12:28:08 +0200

    changed component order and removed comments

--- src/components/SportsPage.jsx
+++ src/components/SportsPage.jsx
@@ -10,41 +10,4 @@ function SportsPage() {
   );
 }
 
-// class SportsPage extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       sport: '',
-//       id: null
-//     };
-//     this.getSport = this.getSport.bind(this);
-//   }
-//   getSport() {
-//     axios
-//       .get('https://sports.api.decathlon.com/sports')
-//       .then(response => response.data.data[1])
-//       .then(data => {
-//         const { id, attributes } = data;
-//         this.setState({
-//           sport: attributes.name,
-//           id,
-//         });
-//       });
-
-//   render() {
-//     const { sport } = this.state;
-//     const { id } = this.state;
-//     return (
-//       <div>
-//         <button type="button" onClick={this.getSport}>
-//           New Sport
-//         </button>
-//         <DivStructure />
-//         <p>{sport}</p>
-//         <p>{id}</p>
-//       </div>
-//     );
-//   }
-// }
-
 export default SportsPage;
