68fe64e9ea60fa4d84f0cdaa29c16ef7ad7f5160
Guigzzz31 <guillaume.trouche.6@gmail.com>
2019-11-04 14:46:42 +0100

    geoloc and searchbar fusioned

--- src/components/MapTest.jsx
+++ src/components/MapTest.jsx
@@ -1,33 +1,63 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import { Map, CircleMarker, TileLayer, Tooltip } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { Map, CircleMarker, TileLayer, Tooltip, Marker, Popup } from 'react-leaflet';
+//import { Card, CardImg, CardText, CardBody, CardLink, CardTitle, CardSubtitle } from 'reactstrap';
 import './MapTest.css';
 import data from './Data';
 
+const myIcon = L.icon({
+  iconUrl: 'https://image.noelshack.com/fichiers/2019/44/3/1572430557-logomap.png',
+  iconSize: [25, 41],
+  iconAnchor: [12.5, 41],
+  popupAnchor: [0, -41]
+});
+
 class MapTest extends React.Component {
+  state = {
+    location: {
+      lat: 51.505,
+      lng: -0.09
+    },
+    haveUsersLocation: false,
+    zoom: 2
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        location: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        },
+        haveUsersLocation: true,
+        zoom: 13
+      });
+    });
+  }
+
   render() {
+    const position = [this.state.location.lat, this.state.location.lng];
+    const isUserLocation = this.state.haveUsersLocation;
+    const voilaZoom = isUserLocation ? this.state.zoom : this.props.zoomCity;
+    const voilaCenter = isUserLocation ? position : this.props.cityCenter;
     return (
       <div>
         <h3 style={{ textAlign: 'center' }} />
-        <Map className="map" zoom={this.props.zoomCity} center={this.props.cityCenter}>
-          <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          {data.city.map(i => {
-            return (
-              <CircleMarker
-                center={[i.coordinates[1], i.coordinates[0]]}
-                icon={{
-                  iconUrl:
-                    'https://www.shutterstock.com/image-vector/simple-red-map-pin-shadow-concept-553653676',
-                  iconSize: [38, 95],
-                  iconAnchor: [i.coordinates[1], i.coordinates[0]]
-                }}
-              >
-                <Tooltip direction="right" offset={[-8, -2]} opacity={1}>
-                  <span>{`Name: ${i.name}, Sport: ${i.sport}`}</span>
-                </Tooltip>
-              </CircleMarker>
-            );
-          })}
+        <Map className="map" zoom={voilaZoom} center={voilaCenter}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {this.state.haveUsersLocation ? (
+            <Marker position={position} icon={myIcon}>
+              <Popup>You are here mothafucka</Popup>
+            </Marker>
+          ) : (
+            ''
+          )}
         </Map>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'leaflet/dist/leaflet.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
