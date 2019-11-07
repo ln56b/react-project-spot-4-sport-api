a422a286314a8a18004b55de0876965c39a10ae9
Vi <dupre.virginie@gmail.com>
2019-11-07 11:19:27 +0100

    Sport List display ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
-// import HomePage from './components/HomePage';
 import SportsPage from './components/SportsPage';
-// import MapPage from './components/MapPage';
-// import NavBar from './components/NavBar';
-// import FirstLetterDivStructure from './components/FirstLetterDivStructure';
+import MapPage from './components/MapPage';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
-      {/* <NavBar />
-      <HomePage />
-      <MapPage /> */}
+      <NavBar />
+      <MapPage />
       <SportsPage />
-      {/* <FirstLetterDivStructure /> */}
     </div>
   );
 }
