3e315707df7d5dfbe28ea9d5e8882d62aacf5f7e
Camille Coin <camillecoin.cc@gmail.com>
2019-11-14 14:51:15 +0100

    changed css

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer2 from './Footer2';
 import './AboutUs.css';
 import WCSLogo from './images/wcsLogo.png';
 import JSLogo from './images/jsLogo.png';
@@ -10,55 +11,62 @@ import CamilleProfilePic from './images/CamilleProfilePic.jpeg';
 
 function AboutUs() {
   return (
-    <div className="about-us-container">
-      <div className="about-us-text">
-        <h2>Wild Code School</h2>
-        <div className="intro-container">
-          <p className="wcs-description">
-            As developers following a 5-months course, we needed to develop a web application for
-            our second project. We needed to use an API that listed sports so that we coud create a
-            localization app.
-            <p>
-              <b>Thus, Spot4Sport was created.</b>
+    <div>
+      <div className="about-us-container">
+        <div className="about-us-text">
+          <h2>Wild Code School</h2>
+          <div className="intro-container">
+            <p className="wcs-description">
+              As developers following a 5-months course, we needed to develop a web application for
+              our second project. We needed to use an API that listed sports so that we coud create
+              a localization app.
+              <p>
+                <b>Thus, Spot4Sport was created.</b>
+              </p>
             </p>
-          </p>
-          <img src={WCSLogo} alt="wild code school logo" />
-        </div>
-      </div>
-      <div className="about-us-technologies">
-        <h2>Technologies used</h2>
-        <div className="techno-first-line">
-          <div className="js-paragraph">
-            <img className="tech-logos" src={JSLogo} alt="javascript logo" />
-            <p>JavaScript</p>
-          </div>
-          <div className="react-paragraph">
-            <img className="tech-logos" src={ReactLogo} alt="react logo" />
-            <p>React JS</p>
+            <img src={WCSLogo} alt="wild code school logo" />
           </div>
         </div>
-        <div className="techno-second-line">
-          <img className="tech-logos" src={GitLogo} alt="git logo" />
-          <p>Git</p>
-          <img className="tech-logos" src={GitHubLogo} alt="github logo" />
+        <div className="about-us-technologies">
+          <h2>Technologies used</h2>
+          <div className="techno-first-line">
+            <div className="js-paragraph">
+              <img className="tech-logos" src={JSLogo} alt="javascript logo" />
+              <p>JavaScript</p>
+            </div>
+            <div className="react-paragraph">
+              <img className="tech-logos" src={ReactLogo} alt="react logo" />
+              <p>React JS</p>
+            </div>
+          </div>
+          <div className="techno-second-line">
+            <div className="git-paragraph">
+              <img className="tech-logos" src={GitLogo} alt="git logo" />
+              <p>Git</p>
+            </div>
+            <div className="github-paragraph">
+              <img className="tech-logos" src={GitHubLogo} alt="github logo" />
+              <p>GitHub</p>
+            </div>
+          </div>
+        </div>
+        <div className="about-us-links">
+          <h2>More information</h2>
+          <img src={HeleneProfilePic} alt="Helene profile pic" />
+          <p>LinkedIn</p>
           <p>GitHub</p>
-        </div>
-      </div>
-      <div className="about-us-links">
-        <h2>More information</h2>
-        <img src={HeleneProfilePic} alt="Helene profile pic" />
-        <p>LinkedIn</p>
-        <p>GitHub</p>
-        <img src={CamilleProfilePic} alt="Camille profile pic" />
-        <p>LinkedIn</p>
-        <p>GitHub</p>
-        {/* <img src={} alt="Virginie profile pic" />
+          <img src={CamilleProfilePic} alt="Camille profile pic" />
+          <p>LinkedIn</p>
+          <p>GitHub</p>
+          {/* <img src={} alt="Virginie profile pic" />
         <p>LinkedIn</p>
         <p>GitHub</p>
         <img src={} alt="Guillaume profile pic" />
         <p>LinkedIn</p>
         <p>GitHub</p> */}
+        </div>
       </div>
+      <Footer2 />
     </div>
   );
 }
