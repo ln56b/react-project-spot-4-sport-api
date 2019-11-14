5a0da0b5e64558f7d9ee541d062fdeb8750c5226
Vi <dupre.virginie@gmail.com>
2019-11-14 13:54:43 +0100

    NavBar and footer last changes for beautiful styling on every component

---
+++ src/components/Footer2.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer2.css';
+import logoTwitter from './images/logoTwitter.png';
+import logoFacebook from './images/logoFacebook.png';
+
+function Footer2() {
+  return (
+    <div className="footer-container2">
+      <span>© Wild Code School, 2019.</span>
+      <div className="logo-container2">
+        <img id="twitter-logo" src={logoTwitter} alt="twitter logo" />
+        <img id="facebook-logo" src={logoFacebook} alt="facebook logo" />
+      </div>
+    </div>
+  );
+}
+
+export default Footer2;
